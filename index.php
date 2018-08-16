84da369dfe79b7bc801f5ecc6d98e8f6d6e4e96d
robertnoract <42448087+robertnoract@users.noreply.github.com>
2018-08-16 11:44:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://us.norton.com/norton-security-for-one-device?inid=nortoncom_nav_norton-security-for-one-device_products-services:home");
 die();
 ?>
